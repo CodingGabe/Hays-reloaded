70cd0d0f938d41595d6be69eb380bec595d62b2c
Gabe Amaya <GabeAmaya2@gmail.com>
2017-07-07 08:38:59 -0500

    setup workflow

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+// I gots sass being compiled, then minified and saved live, and also live reloading with browser synce
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssnano = require('gulp-cssnano');
+var sourcemaps = require('gulp-sourcemaps');
+var browserSync = require('browser-sync').create();
+
+gulp.task('workflow', function () {
+	gulp.src('./src/sass/**/*.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(cssnano())
+		.pipe(sourcemaps.write('./'))
+		.pipe(browserSync.stream())
+
+	.pipe(gulp.dest('./dist/css/'))
+});
+
+// browser sync watches any change to the html and sass injection and auto loads the local browser
+gulp.task('serve', ['workflow'], function() {
+
+    browserSync.init({
+        server: "./"
+    });
+
+    gulp.watch('./src/sass/**/*.scss', ['sass']);
+    gulp.watch('/*.html').on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['workflow','serve']);
